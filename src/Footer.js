a8be134da52170199f379bb3988eba121f46e7fb
yogita pandurang chaudhari <80738479+yogitach01@users.noreply.github.com>
2021-09-30 13:37:45 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p st>Ⓒ Yogita Chaudhari 2021</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Products/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  ProductsContainer,
+  ProductWrapper,
+  ProductsHeading,
+  ProductTitle,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductDesc,
+  ProductPrice,
+  ProductButton
+} from './ProductsElements';
+
+const Products = ({ heading, data }) => {
+  return (
+    <ProductsContainer>
+      <ProductsHeading>{heading}</ProductsHeading>
+      <ProductWrapper>
+        {data.map((product, index) => {
+          return (
+            <ProductCard key={index}>
+              <ProductImg src={product.img} alt={product.alt} />
+              <ProductInfo>
+                <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <ProductButton>{product.button}</ProductButton>
+              </ProductInfo>
+              <p>Ⓒ Yogita Chaudhari 2021</p>
+              
+            </ProductCard>
+           
+          );
+        })}
+      </ProductWrapper>
+    </ProductsContainer>
+  );
+};
+
+export default Products;
